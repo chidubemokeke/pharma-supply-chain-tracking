509d6163569b081f9ef0818796e7c8a3e617571b
chidubemokeke <92036409+chidubemokeke@users.noreply.github.com>
2024-05-25 22:04:29 +0100

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express"); // Import Express framework
 const SensorSimulator = require("./sensorSimulator"); // Import the sensor simulator
 const { ethers } = require("ethers"); // Import ethers.js for interacting with the blockchain
-require("dotenv").config(); // Load environment variables
+require("dotenv").config(); // Load environment variables from .env file
 
 const app = express(); // Initialize Express app
 const port = process.env.PORT || 3000; // Set port from environment variables or default to 3000
@@ -27,7 +27,7 @@ app.post("/sensor-data", async (req, res) => {
 async function recordTemperature(batchId, temperature) {
   const provider = new ethers.providers.InfuraProvider(
     "rinkeby",
-    process.env.INFURA_PROJECT_ID
+    process.env.INFURA_PROJECT_URL
   ); // Connect to Infura
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider); // Create wallet instance from private key
   const contractAddress = process.env.CONTRACT_ADDRESS; // Smart contract address
